d673ef997328fcb0925dc2e091064f7c722db9b1
batuky <imbatuhankaya@gmail.com>
2023-11-29 20:30:43 +0300

    Created mock users endpoint

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+from os import environ
+import random
+import string
+
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
+# db model
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -18,7 +22,7 @@ class User(db.Model):
 
 db.create_all()
 
-#create a test route
+# create a test route
 @app.route('/test', methods=['GET'])
 def test():
   return make_response(jsonify({'message': 'test route'}), 200)
@@ -33,7 +37,7 @@ def create_user():
     db.session.add(new_user)
     db.session.commit()
     return make_response(jsonify({'message': 'user created'}), 201)
-  except e:
+  except Exception as e:
     return make_response(jsonify({'message': 'error creating user'}), 500)
 
 # get all users
@@ -42,7 +46,7 @@ def get_users():
   try:
     users = User.query.all()
     return make_response(jsonify([user.json() for user in users]), 200)
-  except e:
+  except Exception as e:
     return make_response(jsonify({'message': 'error getting users'}), 500)
 
 # get a user by id
@@ -53,7 +57,7 @@ def get_user(id):
     if user:
       return make_response(jsonify({'user': user.json()}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except e:
+  except Exception as e:
     return make_response(jsonify({'message': 'error getting user'}), 500)
 
 # update a user
@@ -68,7 +72,7 @@ def update_user(id):
       db.session.commit()
       return make_response(jsonify({'message': 'user updated'}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except e:
+  except Exception as e:
     return make_response(jsonify({'message': 'error updating user'}), 500)
 
 # delete a user
@@ -81,5 +85,24 @@ def delete_user(id):
       db.session.commit()
       return make_response(jsonify({'message': 'user deleted'}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except e:
+  except Exception as e:
     return make_response(jsonify({'message': 'error deleting user'}), 500)
+  
+# create mock users
+@app.route('/mockusers', methods=['POST'])
+def create_mock_users():
+    try:
+        num_users = 100  # number of created users
+        created_users = []
+
+        for _ in range(num_users):
+            username = ''.join(random.choices(string.ascii_letters, k=8)) 
+            email = f'{username.lower()}@example.com'
+            new_user = User(username=username, email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            created_users.append({'username': username, 'email': email})
+
+        return make_response(jsonify({'message': f'{num_users} users created', 'users': created_users}), 201)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error creating users', 'error': str(e)}), 500)
